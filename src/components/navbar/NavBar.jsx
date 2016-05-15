4d164f7a8ec70c4d1ad4d17842ade746221da41e
oanylund <ole.a.nylund@gmail.com>
2016-05-15 11:29:24 +0200

    Fixed actual router links in navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router';
 
 class NavBar extends React.Component {
   render () {
@@ -7,15 +9,18 @@ class NavBar extends React.Component {
       <Navbar>
          <Navbar.Header>
            <Navbar.Brand>
-             <a href="#">Hackerspace UI</a>
+             <Link to='/'>Hackerspace UI</Link>
            </Navbar.Brand>
          </Navbar.Header>
          <Nav>
            <NavDropdown eventKey={3} title="Medlemmer" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Liste</MenuItem>
+              <LinkContainer to='/medlem/liste'>
+                <MenuItem eventKey={3.1}>Liste</MenuItem>
+              </LinkContainer>
+              <LinkContainer to='/medlem/legg-til'>
               <MenuItem eventKey={3.2}>Legg til</MenuItem>
+              </LinkContainer>
            </NavDropdown>
-           <NavItem eventKey={2} href="#">Status lås</NavItem>
          </Nav>
        </Navbar>
     );
