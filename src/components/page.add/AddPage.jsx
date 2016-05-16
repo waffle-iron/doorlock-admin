1e1db7632ff27f2a101df3cf789cdb79ccf259da
oanylund <ole.a.nylund@gmail.com>
2016-05-16 05:00:27 +0200

    Created simple insert new member, no error handling done yet. starting new branch

--- src/components/page.add/AddPage.jsx
+++ src/components/page.add/AddPage.jsx
@@ -1,13 +1,23 @@
 import React, { PropTypes } from 'react'
 import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 import MemberForm from '../member.form/MemberForm.jsx';
 
 class AddPage extends React.Component {
+  onAddMember(newMember) {
+    axios.post('http://doorlock/api/user/add?token='+localStorage.token, newMember)
+    .then( (response) => {
+      debugger
+    })
+    .catch( (error) => {
+      debugger
+    });
+  }
   render () {
     return (
       <Row>
         <Col md={6} >
-          <MemberForm />
+          <MemberForm submit={this.onAddMember} />
         </Col>
       </Row>
     );
