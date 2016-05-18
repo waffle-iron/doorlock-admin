133e323fd0b65801b4634114c98b94c9c8bae599
oanylund <ole.a.nylund@gmail.com>
2016-05-19 01:25:46 +0200

    Fixed member form story to work after editmode rebuild

--- src/components/.stories/Member.Form.jsx
+++ src/components/.stories/Member.Form.jsx
@@ -6,14 +6,64 @@ import CenterModule from './CenterModule';
 storiesOf('Member.Form', module)
   .add('Add member', () => {
     const props = {
-      submit: action('Form submitted')
+      submit: action('Form submitted'),
+      actions: {
+        scanCard: action('Scan new studentCardId')
+      },
+      studentIdProps: {
+        isLoading: false,
+      }
+    }
+    return <CenterModule width={800} bgColor='#FFF'><MemberForm {...props} /></CenterModule>
+  })
+  .add('Add member, card loading', () => {
+    const props = {
+      submit: action('Form submitted'),
+      actions: {
+        scanCard: action('Scan new studentCardId')
+      },
+      studentIdProps: {
+        isLoading: true,
+      }
     }
     return <CenterModule width={800} bgColor='#FFF'><MemberForm {...props} /></CenterModule>
   })
   .add('Edit member', () => {
     const props = {
+      changeMode: true,
       submit: action('Form submitted'),
-      editMember: {
+      actions: {
+        scanCard: action('Scan new studentCardId')
+      },
+      studentIdProps: {
+        isLoading: false,
+        studId: '242342342'
+      },
+      defaultValues: {
+        firstName: 'Ole-Andreas',
+        lastName: 'Nylund',
+        userName: 'ony008',
+        privateEmail: 'ole@gmail.com',
+        mobile: '48222233',
+        studentCardId: 'e32423424'
+      }
+    }
+
+    return <CenterModule width={800} bgColor='#FFF'><MemberForm {...props}/></CenterModule>
+  })
+  .add('Edit member loading', () => {
+    const props = {
+      changeMode: true,
+      isLoading: true,
+      submit: action('Form submitted'),
+      actions: {
+        scanCard: action('Scan new studentCardId')
+      },
+      studentIdProps: {
+        isLoading: false,
+        studId: '242342342'
+      },
+      defaultValues: {
         firstName: 'Ole-Andreas',
         lastName: 'Nylund',
         userName: 'ony008',
