0a232283499473e6a638ba844a5b10372c210192
oanylund <ole.a.nylund@gmail.com>
2016-06-14 23:21:52 +0200

    Fixed loading screen on StatusLog component

--- src/components/status.loglist/StatusLog.jsx
+++ src/components/status.loglist/StatusLog.jsx
@@ -5,24 +5,37 @@ import styles from './StatusLog-style.css';
 
 class StatusLog extends React.Component {
   componentWillUpdate(nextProps, nextState) {
-    const node = this.refs.scrollBox;
-    this.shouldScrollBottom = node.scrollTop + node.offsetHeight === node.scrollHeight;
+    if(this.props.store.log.length) {
+      const node = this.refs.scrollBox;
+      this.shouldScrollBottom = node.scrollTop + node.offsetHeight === node.scrollHeight;
+    }
   }
   componentDidUpdate(prevProps, prevState) {
-    if (this.shouldScrollBottom) {
+    if (this.shouldScrollBottom && this.props.store.log.length) {
       const node = this.refs.scrollBox;
       node.scrollTop = node.scrollHeight;
     }
   }
+  renderBox() {
+    const log = this.props.store.log;
+    if( !log.length ) {
+      return (<i className='fa fa-cog fa-spin fa-3x fa-fw'></i>);
+    }
+    return (
+      <div ref='scrollBox' className={styles.logBox}>
+        {this.props.store.log.split('\n').map(function(line, i, arr) {
+          if( i === arr.length-1) return null;
+          return (<span key={i}>{line}<br/></span>);
+        })}
+      </div>
+    );
+
+  }
   render () {
+
     return (
       <Panel header={<h3>Server logg</h3>} bsStyle='primary'>
-        <div ref='scrollBox' className={styles.logBox}>
-          {this.props.store.log.split('\n').map(function(line, i, arr) {
-            if( i === arr.length-1) return null;
-            return (<span key={i}>{line}<br/></span>);
-          })}
-        </div>
+        {this.renderBox()}
       </Panel>
     );
   }
