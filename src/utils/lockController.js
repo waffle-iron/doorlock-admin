2f6284ee56b3e69c9d0a5c73c8314f6f0e4ad3dd
oanylund <ole.a.nylund@gmail.com>
2016-06-16 15:56:42 +0200

    Added some error notifications to authsocket on connection error. Added authenticate method to lockcontroller that emits new auth event to server

--- src/utils/lockController.js
+++ src/utils/lockController.js
@@ -1,8 +1,10 @@
 import StatusActions from '../actions/StatusActions';
+import NotificationActions from '../actions/NotificationActions';
 import io from 'socket.io-client';
 import { baseUrl } from 'config';
 
 const authSocket = io(baseUrl + ':8080/auth');
+authSocket.io.reconnectionAttempts(5);
 
 let _isAuthenticated = false;
 
@@ -18,6 +20,9 @@ const LockController = {
   },
   _checkAuth() {
     return _isAuthenticated;
+  },
+  authenticate() {
+    authSocket.emit('authenticate', { token: localStorage.token });
   },
   forceOpen() {
     if( this._checkAuth() ) {
@@ -46,21 +51,34 @@ authSocket.on('connect', () => {
 
   });
 
-  // Initial authentication attempt
-  authSocket.emit('authenticate', { token: localStorage.token });
+  // Initial authentication attempt if localStorage is present
+  if ( localStorage.token ) {
+    authSocket.emit('authenticate', { token: localStorage.token });
+  }
 
   // Socket error handling
   authSocket.on('unauthorized', (err) => {
     if (err.data.type == 'UnauthorizedError' || err.data.code == 'invalid_token') {
       LockController._setAuthentication(false);
-      // console.log('socket.io: attemtping new authentication');
-      // authSocket.emit('authenticate', { token: localStorage.token }); <--- neverending loop
-      // Redirect user to login page perhaps?
-      // Token will be verified by router and should redirect for us..
+      console.log('Authentication of socket failed. Try to refresh page');
+      // Token will be verified by router and should redirect
     }
-    // TODO: Handle reauth when failed. call method from auth util i guess.
-    console.log('authSock error. Stack:');
-    console.log(err);
+  });
+});
+
+authSocket.on('connect_error', () => {
+  NotificationActions.warning({
+    title: 'Tilkoblingsfeil dørlås',
+    message: 'Fikk ikke kontakt med dørlåsserver. Prøver på nytt..',
+    autoDismiss: 2
+  });
+});
+
+authSocket.on('reconnect_failed', () => {
+  NotificationActions.error({
+    title: 'Tilkoblingsfeil dørlås',
+    message: 'Får ikke kontakt med dørlåsserver',
+    autoDismiss: 0
   });
 });
 
