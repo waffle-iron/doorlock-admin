db85c4582b6ea4686712bb2cfe13640f1cb2d72f
oanylund <ole.a.nylund@gmail.com>
2016-09-24 19:25:43 +0200

    Made navbars color blue, and made it responsive

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -6,20 +6,23 @@ import { Link } from 'react-router';
 class NavBar extends React.Component {
   render () {
     return(
-      <Navbar>
+      <Navbar inverse>
          <Navbar.Header>
            <Navbar.Brand>
              <Link to='/status'>Hackerspace UI</Link>
            </Navbar.Brand>
+           <Navbar.Toggle />
          </Navbar.Header>
-         <Nav>
-          <LinkContainer to='/medlem/liste'>
-            <NavItem eventKey={1}>Medlemmer</NavItem>
-          </LinkContainer>
-          <LinkContainer to='/medlem/legg-til'>
-            <NavItem eventKey={2}>Legg til medlem</NavItem>
-          </LinkContainer>
-         </Nav>
+         <Navbar.Collapse>
+           <Nav>
+            <LinkContainer to='/medlem/liste'>
+              <NavItem eventKey={1}>Medlemmer</NavItem>
+            </LinkContainer>
+            <LinkContainer to='/medlem/legg-til'>
+              <NavItem eventKey={2}>Legg til medlem</NavItem>
+            </LinkContainer>
+           </Nav>
+       </Navbar.Collapse>
        </Navbar>
     );
   }
