4259daecd328c71e9a9803854ad602b551ce5ac1
oanylund <ole.a.nylund@gmail.com>
2016-10-22 16:08:32 +0200

    Rebuilt member.list.item storybook with new addons

--- src/components/member.list.item/MemberListItem.story.jsx
+++ src/components/member.list.item/MemberListItem.story.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import MemberListItem from './MemberListItem';
+import { containerWidth } from 'storybook-decorators';
 import { storiesOf, action } from '@kadira/storybook';
+import { withKnobs, text, number } from '@kadira/storybook-addon-knobs';
 
 storiesOf('MemberListItem', module)
+  .addDecorator(containerWidth(500))
+  .addDecorator(withKnobs)
   .add('Member Item', () => {
     const props = {
       onDelete: action('Delete member'),
-      name: 'Ole-Andreas Nylund',
+      name: text('name', 'Ole-Andreas Nylund'),
       id: 2
     }
 
