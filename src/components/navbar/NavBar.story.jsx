071f0ec8557550c8a6d0254829930a01df8390a1
oanylund <ole.a.nylund@gmail.com>
2016-10-22 13:58:29 +0200

    Fixed import bug, imported Navbar when comp name was NavBar

--- src/components/navbar/NavBar.story.jsx
+++ src/components/navbar/NavBar.story.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from './Navbar';
+import NavBar from './NavBar';
 import { storiesOf, action } from '@kadira/storybook';
 import CenterModule from '../.stories/CenterModule.jsx';
 
