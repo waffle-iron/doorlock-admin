6575d5a356b63931f5b3b50ca0d7f0d7ccc2fb73
oanylund <ole.a.nylund@gmail.com>
2016-10-27 00:37:36 +0200

    Rebuilt NavBar component to take in searchField prop to inject a connected component to search whatever the router is showing

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -3,31 +3,28 @@ import { Navbar, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router';
 
-class NavBar extends React.Component {
-  render () {
-    return(
-      <Navbar inverse fixedTop>
-         <Navbar.Header>
-           <Navbar.Brand>
-             <Link to='/lock-status'>Hackerspace UI</Link>
-           </Navbar.Brand>
-           <Navbar.Toggle />
-         </Navbar.Header>
-         <Navbar.Collapse>
-           <Nav>
-             <NavDropdown eventKey={1} title='Medlemmer' id='medlemmer-dropdown'>
-               <LinkContainer to='/medlem/liste'>
-                 <MenuItem eventKey={1.1}>Medlemsliste</MenuItem>
-               </LinkContainer>
-               <LinkContainer to='/medlem/legg-til'>
-                 <MenuItem eventKey={1.2}>Legg til medlem</MenuItem>
-               </LinkContainer>
-             </NavDropdown>
-           </Nav>
-       </Navbar.Collapse>
-       </Navbar>
-    );
-  }
-}
+const NavBar = ({searchField}) => (
+  <Navbar inverse fixedTop>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to='/lock-status'>Hackerspace UI</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      {!!searchField && searchField}
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavDropdown eventKey={1} title='Medlemmer' id='medlemmer-dropdown'>
+          <LinkContainer to='/medlem/liste'>
+            <MenuItem eventKey={1.1}>Medlemsliste</MenuItem>
+          </LinkContainer>
+          <LinkContainer to='/medlem/legg-til'>
+            <MenuItem eventKey={1.2}>Legg til medlem</MenuItem>
+          </LinkContainer>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+)
 
 export default NavBar;
